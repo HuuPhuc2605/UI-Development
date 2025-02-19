42c9414c2b09baaf364a24fd255958129e638d45
HuuPhuc2605 <lehuuphuc6573@gmail.com>
2025-02-20 00:34:48 +0700

    update my-app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,52 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BaiTap_1 from './component/BaiTap_1'
+import BaiTap_2 from './component/BaiTap_2'
+import BaiTap_3 from './component/BaiTap_3'
+import BaiTap_4 from './component/BaiTap_4'
 
 function App() {
-  var styles = {
-    button:{
-      backgroundColor: 'red'
-    }
-  }
-  
- 
-  const [name, setName] = useState('');
-  const [text, setText] = useState('');
-  
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(0);
-  const [result, setResult] = useState(0);
-
-  function handleChange(e){
-    setName(e.target.value);
-  }
-
-  function handleChangeA(e){
-    setA(e.target.value);
-  }
-  function handleChangeB(e){
-    setB(e.target.value);
-  }
-  function handleClick(){
-    setResult(parseInt(a) + parseInt(b));
-  }
-  return (
+  return(
     <>
-    <h2>BaiTap1</h2>
-     <input onChange={handleChange} type="text" />
-     <br/>
-     <button style={styles.button}onClick={handleClick}>Click</button>
-     <br/>
-     <span>{text}</span>
-     <br/>
-     <h2>BaiTap2</h2>
-     <input placeholder = 'nhap a'onChange={handleChangeA} type="text" />
-     <br/>
-     <input placeholder = 'nhap b'onChange={handleChangeB} type="text" />
-     <br/>
-     <button style={styles.button}onClick={handleClick}>Calculate</button>
-     <br/>
-     <span>{result}</span>
+    <BaiTap_1></BaiTap_1>
+    <hr/>
+    <BaiTap_2></BaiTap_2>
+    <hr/>
+    <BaiTap_3></BaiTap_3>
+    <hr/>
+    <BaiTap_4></BaiTap_4>
+  
     </>
   )
 }
