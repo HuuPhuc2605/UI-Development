7870b40d16900bc1ced3226b4b1701ca3517cee6
HuuPhuc2605 <lehuuphuc6573@gmail.com>
2025-03-18 19:40:08 +0700

    update Lab_5

--- my-app/Lab_5/src/App.jsx
+++ my-app/Lab_5/src/App.jsx
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react";
-import AddRecipeForm from "./component/AddRecipeForm";
+import React from "react";
+import Chefity from "./components/Chefity";
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
-
-  useEffect(() => {
-    fetch("https://67ca6b86102d684575c5483b.mockapi.io/Lab4")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("📌 Dữ liệu API:", data); // Kiểm tra API trả về gì
-        setRecipes(data);
-      })
-      .catch((error) => console.error("❌ Lỗi API:", error));
-  }, []);
-
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center mb-6">Danh sách món ăn</h1>
-      {recipes.length >= 4 ? (
-        <AddRecipeForm recipes={recipes} />
-      ) : (
-        <p className="text-center text-gray-500">Đang tải dữ liệu...</p>
-      )}
+    <div>
+      <Chefity />
     </div>
   );
 }
