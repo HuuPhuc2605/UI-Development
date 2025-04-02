890e4748d64768c3e9feaad436d7b9347b43fa01
H82M19\Student <Student>
2025-04-02 10:21:15 +0700

    update

--- my-app/Lab_6/src/App.jsx
+++ my-app/Lab_6/src/App.jsx
@@ -1,28 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import UseReducerComponent from "./components/UseReducerComponent";
 import UseRefComponent from "./components/UseRefComponent";
-import HomePage from "./pages/HomePage";
-import RootPage from "./components/RootPage";
+
 import UseEffectComponent from "./components/UseEffectComponent";
 import Memomo from "./components/Memomo";
 import UseCallbackComponent from "./components/UseCallbackComponent";
 import UseMemoComponent from "./components/UseMemoComponent";
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <RootPage />,
-    children: [
-      { path: "", element: <HomePage /> },
-      { path: "/reducer", element: <UseReducerComponent /> },
-      { path: "/effect", element: <UseEffectComponent /> },
-      { path: "/ref", element: <UseRefComponent /> },
-      { path: "/memo", element: <Memomo /> },
-      { path: "/callback", element: <UseCallbackComponent /> },
-      { path: "/usememo", element: <UseMemoComponent /> },
-    ],
-  },
-]);
 function App() {
-  return <RouterProvider router={router} />;
+
+
+  return (
+    <div>
+    <UseReducerComponent/>
+    <UseEffectComponent/>
+    <UseRefComponent/>
+    <UseCallbackComponent/>
+    <UseMemoComponent/>
+    <Memomo/>
+    </div>
+  )
 }
-export default App;
+
+export default App
+
---
+++ my-app/Lab_7/src/components/Grid.jsx
@@ -0,0 +1,15 @@
+const Grid = () => {
+    return (
+      <div className="grid grid-cols-4 p-4 gap-4 bg-gray-100  bg-blue-400">
+        <div className="bg-blue-500 p-6 rounded-lg text-white text-center">Item 1</div>
+        <div className="bg-blue-500 p-6 rounded-lg text-white text-center">Item 2</div>
+        <div className="bg-blue-500 p-6 rounded-lg text-white text-center">Item 3</div>
+        <div className="bg-blue-500 p-6 rounded-lg text-white text-center">Item 4</div>
+        <div className="bg-blue-500 p-6 rounded-lg text-white text-center">Item 5</div>
+        <div className="bg-blue-500 p-6 rounded-lg text-white text-center">Item 6</div>
+      </div>
+    );
+  };
+  
+  export default Grid;
+  
