8a57d0469c44733a45cfc8005ce18f449391d7a9
HuuPhuc2605 <lehuuphuc6573@gmail.com>
2025-04-16 14:57:09 +0700

    add them duong dan do chinh sua nham trong file Lab_4

---
+++ my-app/Lab_4/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chefity from "./components/Chefity";
+import Lab3 from "./components/SaladPage";
+
+function App() {
+  return (
+    <div>
+      <Chefity />
+    </div>
+  );
+}
+
+export default App;
